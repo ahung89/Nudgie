04e5f340cc7dce479be53b4a4c0db7cfb478116b
Andrew Hung <andrew@legacylabs.io>
2023-10-06 19:44:36 -0500

    removed old celery tut code

--- celery_test.py
+++
@@ -1,9 +0,0 @@
-from tasks import add
-
-result = add.delay(4, 4)
-#result.ready() #this will return False
-print(result.get(timeout=1))
-
-#wait for 3 seconds before requesting the result
-# time.sleep(3)
-# print(result.get(timeout=1)) #this will not work without configuring Celery to use a result backend
