44b3226f413c4c264225280618a77ce3a1c280d6
Andrew Hung <andrew@legacylabs.io>
2023-10-14 14:41:50 -0500

    exclude api call from redirect

--- Nudgie/middleware/auth.py
+++ Nudgie/middleware/auth.py
@@ -5,8 +5,9 @@ class AuthenticationMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+         #eventually you're going to want to modify this condition to exclude all the chatbot api endpoints
         if not request.user.is_authenticated and request.path \
-              not in ['/accounts/login/', '/accounts/signup/']:
+              not in ['/accounts/login/', '/accounts/signup/', '/chatbot/api/']:
             return redirect('/accounts/login/')
         response = self.get_response(request)
         return response
